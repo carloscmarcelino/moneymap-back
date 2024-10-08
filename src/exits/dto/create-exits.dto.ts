e707ad85e0edd4ae435235d5027a482357c9eed5
carlos <carloslima1598@gmail.com>
2024-10-08 19:42:24 -0300

    feat: edit expense date

--- src/exits/dto/create-exits.dto.ts
+++ src/exits/dto/create-exits.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 export class CreateExitsDto {
   @IsString()
@@ -12,4 +12,8 @@ export class CreateExitsDto {
   @IsString()
   @IsNotEmpty()
   paymentMethodId: string;
+
+  @IsOptional()
+  @IsString()
+  date: string;
 }
