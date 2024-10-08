891db6c185ead2355a0c15b15b43aad68ad68b73
carlos <carloslima1598@gmail.com>
2024-10-07 22:33:00 -0300

    fix: broker response

--- src/brokers/brokers.controller.ts
+++ src/brokers/brokers.controller.ts
@@ -1,13 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { Broker } from './types';
 import { BrokersService } from './brokers.service';
+import { ApiResponse } from 'src/types';
 
 @Controller('broker')
 export class BrokersController {
   constructor(private readonly brokersService: BrokersService) {}
 
   @Get()
-  async get(): Promise<Broker[]> {
+  async get(): Promise<ApiResponse<Broker>> {
     return this.brokersService.get();
   }
 }
--- src/brokers/brokers.service.ts
+++ src/brokers/brokers.service.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
 import { Broker } from './types';
 import { Injectable } from '@nestjs/common';
+import { ApiResponse } from 'src/types';
 
 @Injectable()
 export class BrokersService {
-  async get(): Promise<Broker[]> {
+  async get(): Promise<ApiResponse<Broker>> {
     const { data } = await axios.get<Broker[]>(
       'https://brasilapi.com.br/api/cvm/corretoras/v1',
     );
 
-    return data;
+    return {
+      data: data.filter((broker) => broker.status !== 'CANCELADA'),
+      totalItems: data.length,
+    };
   }
 }
